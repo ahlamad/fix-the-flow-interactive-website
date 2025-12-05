15770a54637a6f032694245f46cab8d6c4e7011b
addaahva <ahlam.adda@hva.nl>
2025-12-05 11:52:28 +0100

    #1 eerste interactie (alert)

---
+++ index.js
@@ -0,0 +1,10 @@
+// Selecteer radio input voor toggle van thema
+let darkModeRadio = document.querySelector('.dark-morde-theme')
+
+// EventListener toevoegen aan de radio input
+darkModeRadio.addEventListener('click', function() {
+    alert('dark mode')
+
+// Voeg iets toe zodat de gebruiker ziet dat de darkmode thema wordt toegevoegd
+    document.querySelector('')
+})
