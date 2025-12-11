78c4b4d4603c8ab6823d652a3d4a5e02f85a6d68
ahlamad <ahlam.adda@hva.nl>
2025-12-11 16:38:31 +0100

    #1 close #8 3 stappenplan voor de js en aparte bestanden

---
+++ appearance.js
@@ -0,0 +1,16 @@
+// Selecteer de buttons die je nodig hebt
+let matchButton = document.querySelector('.match-system-button')
+let darkButton = document.querySelector('.dark-mode-button')
+let lightButton = document.querySelector('.light-mode-button')
+
+// Voeg de click event toe aan de buttons met callback functies
+
+
+
+// Functie die de styling van de applicatie verandert door een class toe te voegen, nadat er op geklikt wordt
+
+
+
+
+
+// Button die opslaat wat de gebruiker heeft gekozen met localStorage
